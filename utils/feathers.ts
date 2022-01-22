a84be531ba760c4713f78a46080c655d0f9b8264
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-22 10:48:51 +0300

    Change the configuration of the feather-client. Adding types

--- utils/feathers.ts
+++ utils/feathers.ts
@@ -1,10 +1,17 @@
 import io from "socket.io-client";
-import feathers, { Application } from "@feathersjs/feathers";
+import feathers, { Application, Service } from "@feathersjs/feathers";
 import socketio from "@feathersjs/socketio-client";
+import auth from "@feathersjs/authentication-client";
+
+interface IFeathersClient {
+  notes: Service<any>;
+  users: Service<any>;
+}
 
 const socket: SocketIOClient.Socket = io("https://my-feels-api.herokuapp.com/");
-const feathersClient: Application<any> = feathers();
+const feathersClient: Application<IFeathersClient> = feathers();
 
+feathersClient.configure(auth());
 feathersClient.configure(socketio(socket));
 
 export default feathersClient;
