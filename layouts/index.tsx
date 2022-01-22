875b6d797cd83cc2be0b6d1167bb5116ac7a489e
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-22 17:16:51 +0300

    Adding styles to a layout component

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -2,8 +2,8 @@ import React, { FunctionComponent, ReactNode } from "react";
 
 const TheLayout: FunctionComponent<{ children: ReactNode }> = ({ children }) => {
   return (
-    <main>
-      <div>{children}</div>
+    <main className="flex items-center justify-center px-5 lg:px-0">
+      <div className="w-full h-16 max-w-xl">{children}</div>
     </main>
   );
 };
