f7100db08b39a74bb01abf9c38e1a2969566ef3c
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-22 08:46:00 +0300

    Preparing the Home page for later work

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
-import type { NextPage } from "next";
+import type { NextPage, GetServerSideProps } from "next";
 
-const Home: NextPage = () => {
+const Home: NextPage<IHomePageProps> = props => {
   return <></>;
+};
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  return {
+    props: {},
+  };
 };
 
 export default Home;
