b030b412f9a803acb92d262687f3a557af081272
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-22 11:21:09 +0300

    Fixing the main layout of the application.

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -1,7 +1,11 @@
 import React, { FunctionComponent, ReactNode } from "react";
 
 const TheLayout: FunctionComponent<{ children: ReactNode }> = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <main>
+      <div>{children}</div>
+    </main>
+  );
 };
 
 export default TheLayout;
