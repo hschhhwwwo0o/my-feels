8ed09a5d9bb2870f3bbd4e0955c7d4cc7ff613c2
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-22 08:21:26 +0300

    Adding framer-motion. Global provider for framer-motion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import type { AppProps } from "next/app";
 
+/**
+ * AnimatePresence
+ * https://www.framer.com/docs/animate-presence/
+ */
+import { AnimatePresence } from "framer-motion";
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AnimatePresence>
+      <Component {...pageProps} />
+    </AnimatePresence>
+  );
 }
 
 export default MyApp;
