01f602f4e79eab13758cbbc88d5f8f03b255efe6
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-25 16:46:31 +0300

    Add Secondary Buttoncomponent

---
+++ components/UI/SecondaryButton/index.tsx
@@ -0,0 +1,15 @@
+import React, { FunctionComponent } from "react";
+
+const SecondaryButton: FunctionComponent = ({ children }) => {
+  return (
+    <>
+      <div>
+        <button className="w-full bg-[#FAFAFA] text-[#2F2F2F] py-[21px] rounded-md text-lg px-5 font-semibold border-2 border-[#EAEAEA] dark:border-[#151515] transition-all duration-1000">
+          {children}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default SecondaryButton;
--- pages/create-an-account/index.tsx
+++ pages/create-an-account/index.tsx
@@ -1,10 +1,13 @@
 import type { NextPage } from "next";
+import SecondaryButton from "../../components/UI/SecondaryButton";
 import TheLayout from "../../layouts";
 
 const CreateAnAccount: NextPage = props => {
   return (
     <TheLayout>
-      <div></div>
+      <div>
+        <SecondaryButton>Hello</SecondaryButton>
+      </div>
     </TheLayout>
   );
 };
