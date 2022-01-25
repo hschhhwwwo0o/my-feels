24da7ede4cc5882bac24e18a051f3ff528d3e53a
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-25 17:49:52 +0300

    Add Logo Feedback component

---
+++ components/UI/LogoFeedback/index.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent } from "react";
+
+const LogoFeedback: FunctionComponent = () => {
+  return (
+    <>
+      <div>
+        <h1 className="text-xl lg:text-3xl dark:text-white">
+          <i>my </i>
+          <span className="font-bold">feels </span>
+          <span>feedback</span>
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default LogoFeedback;
