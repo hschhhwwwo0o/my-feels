a986fe85eb0dc9b05d3746f04a1e5ba80e12fde2
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-25 18:36:28 +0300

    Profile Page

--- components/UI/MoreButton/index.tsx
+++ components/UI/MoreButton/index.tsx
@@ -4,7 +4,7 @@ const MoreButton: FunctionComponent = () => {
   return (
     <>
       <div>
-        <button className="w-[61px] h-[61px] border-2 rounded-md flex items-center justify-center border-[#EAEAEA] bg-[#FAFAFA]">
+        <button className="w-[70px] h-[70px] border-2 rounded-md flex items-center justify-center border-[#EAEAEA] bg-[#FAFAFA]">
           <div className="flex flex-row gap-1">
             <div className="w-[5px] h-[5px] bg-[#2F2F2F] rounded-full" />
             <div className="w-[5px] h-[5px] bg-[#2F2F2F] rounded-full" />
--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,7 +1,50 @@
 import type { NextPage } from "next";
+import BrandButton from "../../components/UI/BrandButton";
+import Input from "../../components/UI/Input";
+import MinimalLogo from "../../components/UI/MinimalLogo";
+import MoreButton from "../../components/UI/MoreButton";
+import SecondaryButton from "../../components/UI/SecondaryButton";
+import ThemeSwitch from "../../components/UI/ThemeSwitch";
+import TheLayout from "../../layouts";
 
 const Profile: NextPage<IProfilePageProps> = props => {
-  return <></>;
+  return (
+    <TheLayout>
+      <div className="flex flex-col items-center">
+        <div className="py-12 hidden lg:block">
+          <MinimalLogo />
+        </div>
+        <div className="text-base lg:text-lg underline pb-9 pt-9 lg:pt-0 lg:pb-12">Personal Account Settings</div>
+        <div className="w-full lg:w-96 flex flex-col gap-3">
+          <Input />
+          <Input />
+          <Input />
+          <Input />
+          <div>
+            <div className="text-sm">Theme</div>
+            <div className="mt-3">
+              <ThemeSwitch />
+            </div>
+          </div>
+          <div className="w-full left-0 px-6 lg:px-0 fixed bottom-5 lg:w-full lg:relative lg:bottom-0">
+            <div className="flex flex-col gap-3">
+              <div className="mt-9">
+                <div>
+                  <BrandButton>Save</BrandButton>
+                </div>
+              </div>
+              <div className="flex flex-row gap-3">
+                <div className="w-full">
+                  <SecondaryButton>Cancel</SecondaryButton>
+                </div>
+                <MoreButton />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </TheLayout>
+  );
 };
 
 export default Profile;
