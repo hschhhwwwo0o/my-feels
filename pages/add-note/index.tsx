4101c5310f3970979aa24b1ee0a73003fe3e8e49
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-26 18:32:23 +0300

    Fix header text

--- pages/add-note/index.tsx
+++ pages/add-note/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage<IHomePageProps> = props => {
         <div className="mt-36">
           <div className="font-semibold text-base text-[#3b3b3b] dark:text-[#565656]">Add title:</div>
           <input
-            className="font-semibold w-full text-3xl lg:text-5xl outline-none border-none bg-transparent mt-4 placeholder-[#303030] dark:placeholder-[#959595] dark:text-[#c0c0c0]"
+            className="font-semibold w-full text-3xl lg:text-5xl outline-none border-none bg-transparent mt-4 placeholder-[#303030] dark:placeholder-[#959595] dark:text-[#e7e7e7]"
             type="text"
             placeholder="Enter title..."
           />
