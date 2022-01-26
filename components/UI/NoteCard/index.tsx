2f6b2eaf56d49cb8a50cee4eb14178a83d24cb7c
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-26 14:10:23 +0300

    Add Home page

--- components/UI/NoteCard/index.tsx
+++ components/UI/NoteCard/index.tsx
@@ -4,21 +4,23 @@ const NoteCard: FunctionComponent = () => {
   return (
     <>
       <div>
-        <div className="border-2 border-[#EAEAEA] dark:border-[#2D2D2D] w-full max-w-[262px] h-[262px] rounded-[21px] shadow-md px-6 py-5">
+        <div className="border-2 border-[#EAEAEA] dark:border-[#2D2D2D] w-full max-w-[164px] lg:max-w-[272px] h-[164px] lg:h-[272px] rounded-[21px] shadow-md px-4 lg:px-6 py-4 lg:py-5">
           <div>
-            <h2 className="font-medium text-lg text-ellipsis overflow-hidden whitespace-nowrap text-[#242424] dark:text-[#D8D8D8]">
+            <h2 className="font-medium text-sm lg:text-lg text-ellipsis overflow-hidden whitespace-nowrap text-[#242424] dark:text-[#D8D8D8]">
               My Diary Note
             </h2>
           </div>
-          <div className="mt-3">
-            <p className="text-[#878787] dark:text-[#A5A5A5] text-sm max-h-[140px] overflow-hidden text-justify font-medium">
+          <div className="mt-2 lg:mt-3">
+            <p className="text-[#878787] dark:text-[#A5A5A5] text-[10px] lg:text-sm max-h-[73px] lg:max-h-[140px] overflow-hidden text-justify font-medium">
               “Archive is the publishing house behind the most inspiring books all around Europe. We designed the
               Archive website with some exciting features to give users an even better access to their goodies and we
               also made an improved mobile version.
             </p>
           </div>
           <div>
-            <div className="text-xs text-[#C5C5C5] dark:text-[#A5A5A5] font-semibold mt-6">12/02/2022</div>
+            <div className="text-[10px] lg:text-xs text-[#C5C5C5] dark:text-[#A5A5A5] font-semibold mt-3 lg:mt-6">
+              12/02/2022
+            </div>
           </div>
         </div>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,32 @@
 import type { NextPage } from "next";
+import TheLayout from "layouts";
+import MinimalLogo from "components/UI/MinimalLogo";
+import NoteCard from "components/UI/NoteCard";
 
 const Home: NextPage<IHomePageProps> = props => {
-  return <></>;
+  return (
+    <TheLayout>
+      <div>
+        <div className="flex items-center justify-center py-12">
+          <MinimalLogo />
+        </div>
+        <div className="text-center text-[#242424] dark:text-[#E8E8E8] pb-14 text-lg">Saveliy Meetrofanov</div>
+        <div>
+          <div className="text-[#242424] dark:text-[#E8E8E8] text-sm font-medium">Pinned</div>
+          <div className="grid grid-cols-2 gap-4 lg:gap-8 mt-3">
+            <NoteCard />
+          </div>
+        </div>
+        <div className="mt-12">
+          <div className="text-[#242424] dark:text-[#E8E8E8] text-sm font-medium">All</div>
+          <div className="grid grid-cols-2 gap-4 lg:gap-8 mt-3">
+            <NoteCard />
+            <NoteCard />
+          </div>
+        </div>
+      </div>
+    </TheLayout>
+  );
 };
 
 export default Home;
