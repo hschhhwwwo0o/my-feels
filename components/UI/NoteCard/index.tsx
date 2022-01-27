3713a14e223e23c4d11c2e0a7abe9cf7551a3e36
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-27 21:15:16 +0300

    Some fix redirect from note card

--- components/UI/NoteCard/index.tsx
+++ components/UI/NoteCard/index.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from "react";
-import Link from "next/link";
+import { Link as Anchor } from "react-scroll";
+import { NextRouter, useRouter } from "next/router";
 
 interface INoteCard {
   title?: string;
@@ -9,30 +10,36 @@ interface INoteCard {
 }
 
 const NoteCard: FunctionComponent<INoteCard> = ({ title = "", text = "", date = "", id = "1" }) => {
+  const router: NextRouter = useRouter();
+
+  function redirect() {
+    setTimeout(() => {
+      router.push(`/note/${id}`);
+    }, 610);
+  }
+
   return (
-    <Link href={`/note/${id}`}>
-      <a>
-        <div className="cursor-pointer">
-          <div className="border-2 border-[#EAEAEA] dark:border-[#2D2D2D] w-full max-w-[164px] lg:max-w-[272px] h-[164px] lg:h-[272px] rounded-[21px] shadow-md px-4 lg:px-6 py-4 lg:py-5 transition-all duration-1000">
-            <div>
-              <h2 className="font-medium text-sm lg:text-lg text-ellipsis overflow-hidden whitespace-nowrap text-[#242424] dark:text-[#D8D8D8] transition-all duration-1000">
-                {title}
-              </h2>
-            </div>
-            <div className="mt-2 lg:mt-3">
-              <p className="text-[#878787] dark:text-[#A5A5A5] text-[10px] lg:text-sm min-h-[73px] max-h-[73px] lg:min-h-[140px] lg:max-h-[140px] overflow-hidden text-justify font-medium transition-all duration-1000">
-                {text ? text : "The note contains nothing"}
-              </p>
-            </div>
-            <div>
-              <div className="text-[10px] lg:text-xs text-[#C5C5C5] dark:text-[#A5A5A5] font-semibold mt-3 lg:mt-6 transition-all duration-1000">
-                12/02/2022
-              </div>
+    <Anchor to="#header" smooth={true} duration={600}>
+      <div onClick={redirect} className="cursor-pointer">
+        <div className="border-2 border-[#EAEAEA] dark:border-[#2D2D2D] w-full max-w-[164px] lg:max-w-[272px] h-[164px] lg:h-[272px] rounded-[21px] shadow-md px-4 lg:px-6 py-4 lg:py-5 transition-all duration-1000">
+          <div>
+            <h2 className="font-medium text-sm lg:text-lg text-ellipsis overflow-hidden whitespace-nowrap text-[#242424] dark:text-[#D8D8D8] transition-all duration-1000">
+              {title}
+            </h2>
+          </div>
+          <div className="mt-2 lg:mt-3">
+            <p className="text-[#878787] dark:text-[#A5A5A5] text-[10px] lg:text-sm min-h-[73px] max-h-[73px] lg:min-h-[140px] lg:max-h-[140px] overflow-hidden text-justify font-medium transition-all duration-1000">
+              {text ? text : "The note contains nothing"}
+            </p>
+          </div>
+          <div>
+            <div className="text-[10px] lg:text-xs text-[#C5C5C5] dark:text-[#A5A5A5] font-semibold mt-3 lg:mt-6 transition-all duration-1000">
+              {date}
             </div>
           </div>
         </div>
-      </a>
-    </Link>
+      </div>
+    </Anchor>
   );
 };
 
--- layouts/index.tsx
+++ layouts/index.tsx
@@ -2,9 +2,12 @@ import React, { FunctionComponent, ReactNode } from "react";
 
 const TheLayout: FunctionComponent<{ children: ReactNode }> = ({ children }) => {
   return (
-    <main className="flex items-center justify-center px-5 lg:px-0">
-      <div className="w-full max-w-xl">{children}</div>
-    </main>
+    <>
+      <div id="#header"></div>
+      <main className="flex items-center justify-center px-5 lg:px-0">
+        <div className="w-full max-w-xl">{children}</div>
+      </main>
+    </>
   );
 };
 
