65d2b562f3287770734e173d6da5e68472fbfd77
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-27 16:35:12 +0300

    Initial index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import type { NextPage } from "next";
-import Link from "next/link";
+import { IStore } from "redux";
+import { useSelector } from "react-redux";
 import TheLayout from "layouts";
+import Link from "next/link";
 import MinimalLogo from "components/UI/MinimalLogo";
 import NoteCard from "components/UI/NoteCard";
 import BrandButton from "components/UI/BrandButton";
 import AddCircleButton from "components/UI/AddCircleButton";
 
 const Home: NextPage<IHomePageProps> = props => {
+  const { notes, user }: IStore = useSelector((store: IStore) => store);
+
   return (
     <TheLayout>
       <div className="h-full">
@@ -15,21 +19,34 @@ const Home: NextPage<IHomePageProps> = props => {
         </div>
         <Link href="/profile">
           <a>
-            <div className="text-center text-[#242424] dark:text-[#E8E8E8] pb-14 text-lg">Saveliy Meetrofanov</div>
+            <div className="text-center text-[#242424] dark:text-[#E8E8E8] pb-14 text-lg">
+              {user.firstName} {user.lastName}
+            </div>
           </a>
         </Link>
-        <div>
-          <div className="text-[#242424] dark:text-[#E8E8E8] text-sm font-medium">Pinned</div>
-          <div className="grid grid-cols-2 gap-4 lg:gap-8 mt-3">
-            <NoteCard />
+        {notes.pinnedNotes.length ? (
+          <div>
+            <div className="text-[#242424] dark:text-[#E8E8E8] text-sm font-medium">Pinned</div>
+            <div className="grid grid-cols-2 gap-4 lg:gap-8 mt-3">
+              <NoteCard />
+            </div>
           </div>
-        </div>
-        <div className="mt-12">
-          <div className="text-[#242424] dark:text-[#E8E8E8] text-sm font-medium">All</div>
-          <div className="grid grid-cols-2 gap-3 lg:gap-8 mt-3">
-            <NoteCard />
-            <NoteCard />
+        ) : (
+          ""
+        )}
+        {notes.notes.length ? (
+          <div className="mt-12">
+            <div className="text-[#242424] dark:text-[#E8E8E8] text-sm font-medium">All</div>
+            <div className="grid grid-cols-2 gap-3 lg:gap-8 mt-3">
+              <NoteCard />
+              <NoteCard />
+            </div>
           </div>
+        ) : (
+          ""
+        )}
+        <div className="text-center">
+          {notes.notes.length === 0 && notes.pinnedNotes.length === 0 ? "You don't have notes yet" : ""}
         </div>
         <div className="fixed w-full left-0 px-5 bottom-5 lg:hidden">
           <div className="shadow-2xl">
