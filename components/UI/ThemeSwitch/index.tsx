d1a824fe97848d07eb335f6fe3d8d91a39337ea2
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-27 15:48:34 +0300

    Add default click handler for theme switch

--- components/UI/ThemeSwitch/index.tsx
+++ components/UI/ThemeSwitch/index.tsx
@@ -1,9 +1,16 @@
 import React, { FunctionComponent } from "react";
 
-const ThemeSwitch: FunctionComponent = () => {
+interface IThemeSwitch {
+  onClickHandler?: () => any;
+}
+
+const ThemeSwitch: FunctionComponent<IThemeSwitch> = ({ onClickHandler = () => {} }) => {
   return (
     <>
-      <div className="w-[196px] h-[36px] bg-[#F5F5F5] dark:bg-[#1A1A1A] rounded-lg flex items-center justify-center px-1 overflow-hidden font-semibold transition-all duration-1000">
+      <div
+        className="w-[196px] h-[36px] bg-[#F5F5F5] dark:bg-[#1A1A1A] rounded-lg flex items-center justify-center px-1 overflow-hidden font-semibold transition-all duration-1000"
+        onClick={onClickHandler}
+      >
         <div className="flex items-center justify-center w-full text-[13px]">
           <div className="w-full h-[30px] rounded-lg flex items-center justify-center bg-[#FAFAFA] dark:bg-[#1A1A1A] dark:text-[#F5F5F5] cursor-pointer shadow-md transition-all duration-1000">
             Light
