0dce388e919a121b99b25ed5280d70071c19dc98
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-27 17:09:16 +0300

    Add current note state to redux

--- redux/notes/index.ts
+++ redux/notes/index.ts
@@ -4,32 +4,38 @@ function notesReducer(state = initialNotesStore, action: any): INotesStore {
   switch (action.type) {
     case "SET_NOTES":
       return {
+        ...state,
         notes: action.payload.notes.filter((note: INote) => !note.isPinned),
         pinnedNotes: action.payload.notes.filter((note: INote) => note.isPinned),
       };
     case "REMOVE_NOTE":
       return {
+        ...state,
         notes: state.notes.filter((note: INote) => note._id !== action.payload._id),
         pinnedNotes: state.pinnedNotes.filter((note: INote) => note._id !== action.payload._id),
       };
     case "ADD_NOTE":
       return {
+        ...state,
         notes: [action.payload.note, ...state.notes],
         pinnedNotes: state.pinnedNotes,
       };
     case "PIN_NOTE":
       return {
+        ...state,
         notes: state.notes.filter((note: INote) => note._id !== action.payload._id),
         pinnedNotes: [action.payload.note, ...state.pinnedNotes],
       };
     case "SAVE_NOTE":
       if (action.payload.note.isPinned) {
         return {
+          ...state,
           notes: state.notes,
           pinnedNotes: [action.payload.note, ...state.pinnedNotes],
         };
       } else {
         return {
+          ...state,
           notes: [action.payload.note, ...state.notes],
           pinnedNotes: state.pinnedNotes,
         };
--- redux/notes/initialStore.ts
+++ redux/notes/initialStore.ts
@@ -1,9 +1,20 @@
 export interface INotesStore {
   notes: INote[];
   pinnedNotes: INote[];
+  currentNote: INote;
 }
 
 export const initialNotesStore: INotesStore = {
   notes: [],
   pinnedNotes: [],
+  currentNote: {
+    title: "",
+    text: "",
+    authorID: "",
+    emojies: [],
+    isPinned: false,
+    createdAt: "",
+    updatedAt: "",
+    _id: "",
+  },
 };
