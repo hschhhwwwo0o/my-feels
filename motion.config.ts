e8263096dadc765408da00ce4b4954edc223a900
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-29 13:53:34 +0300

    Fix motion configuration

--- motion.config.ts
+++ motion.config.ts
@@ -1,6 +1,6 @@
 export const MotionConfig = {
-  transitionDuration: 0.54,
+  transitionDuration: 0.24,
   initialOpacity: 0,
   defaultOpacity: 1,
-  delayApplication: 0.1,
+  delayApplication: 0.08,
 };
