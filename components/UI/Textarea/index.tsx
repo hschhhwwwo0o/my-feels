da8a0914eaf22c4a6454dd38f1761a583719640f
Saveliy <ssandry.github@gmail.com>
2022-02-01 17:31:22 +0300

    Chenge textarea height

--- components/UI/Textarea/index.tsx
+++ components/UI/Textarea/index.tsx
@@ -12,7 +12,7 @@ const Textarea: FunctionComponent<ITextarea> = ({ value = "", setValue = () => {
       onChange={e => {
         setValue(e.target.value);
       }}
-      className="mt-5 w-full bg-transparent font-medium text-[#444444] dark:text-[#B2B2B2] outline-none text-justify h-[1000px] placeholder-[#bebebe] dark:placeholder-[#545454]"
+      className="mt-5 w-full bg-transparent font-medium text-[#444444] dark:text-[#B2B2B2] outline-none text-justify h-[3000px] placeholder-[#bebebe] dark:placeholder-[#545454]"
       placeholder="Enter text"
     />
   );
