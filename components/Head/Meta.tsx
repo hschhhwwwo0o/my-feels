6bcada05e331ad55a97847cc44c5278ce2b0fadd
Saveliy <ssandry.github@gmail.com>
2022-02-01 18:02:02 +0300

    Update meta tags

--- components/Head/Meta.tsx
+++ components/Head/Meta.tsx
@@ -4,6 +4,20 @@ const Meta: FunctionComponent = props => {
   return (
     <>
       <title>My Feels</title>
+      <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
+
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta http-equiv="X-UA-Compatible" content="chrome=1" />
+      <meta name="hostname" content="my-feels-web.vercel.app" />
+      <meta httpEquiv="Content-language" content="en-US" />
+      <meta name="robots" content="index, follow" />
+
+      <meta name="distribution" content="global" />
+      <meta name="author" content="@hschhhwwwo0o" />
+      <meta name="publisher" content="@hschhhwwwo0o" />
+      <meta name="copyright" content="@hschhhwwwo0o" />
+
+      <meta name="theme-color" content="#fff" />
     </>
   );
 };
