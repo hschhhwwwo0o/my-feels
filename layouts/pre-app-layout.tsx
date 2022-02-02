a04dc9f0c209d3db4f9bd8ae1abb6585b346e68a
Saveliy <ssandry.github@gmail.com>
2022-02-02 21:30:27 +0300

    Try fix loading user

--- layouts/pre-app-layout.tsx
+++ layouts/pre-app-layout.tsx
@@ -33,7 +33,7 @@ const ThePreApplicationLayout: FunctionComponent<{ children: ReactNode }> = ({ c
 
   return (
     <div className={user.theme}>
-      <div className={`${isLoading ? "opacity-0" : "opacity-100"} transition-all duration-1000`}>
+      <div className={`${isLoading ? "opacity-0" : "opacity-100"} transition-all duration-150`}>
         <div className="bg-[#FAFAFA] min-h-screen dark:bg-[#151515] transition-all duration-1000">
           <div>{children}</div>
         </div>
