4622b4798737b8abde9d74837751bf7a855285cc
Saveliy <ssandry.github@gmail.com>
2022-02-02 19:42:43 +0300

    Optimization z-index

--- components/Modals/Loading/index.tsx
+++ components/Modals/Loading/index.tsx
@@ -6,7 +6,7 @@ const LoadingModal: FunctionComponent<{ isLoading: boolean }> = props => {
     <>
       <div
         className={`w-screen h-screen fixed top-0 left-0 bg-[#FAFAFA] transition-all duration-1000 dark:bg-[#151515] flex items-center justify-center py-20 delay-1000 delay-1000 ${
-          !props.isLoading ? "opacity-0 -z-10" : "z-[10000000000]"
+          !props.isLoading ? "opacity-0 -z-10" : "z-[10000]"
         }`}
       >
         <div className="h-full flex flex-col items-center justify-between">
