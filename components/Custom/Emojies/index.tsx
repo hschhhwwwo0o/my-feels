ab1863ac6dcb6de3f3f24e10f11d6fc970f684f3
Saveliy <ssandry.github@gmail.com>
2022-02-14 20:19:31 +0300

    Fix emojies component - refactoring

--- components/Custom/Emojies/index.tsx
+++ components/Custom/Emojies/index.tsx
@@ -14,49 +14,25 @@ const Emojies: FunctionComponent<IEmojies> = ({ value = [], setValue = () => {} 
     }
   }
 
+  const emojiesArray = [{ name: "cry" }, { name: "love" }, { name: "smile" }, { name: "sparkles" }];
+
   return (
     <>
       <div className="flex flex-row gap-3 items-center relative -top-[1px]">
-        <img
-          onClick={() => {
-            onClickHandler("cry");
-          }}
-          src="/emoji/cry.png"
-          alt=""
-          className={`w-[22px] h-[22px] cursor-pointer transition-all duration-300 filter ${
-            value.includes("cry") ? "" : "grayscale"
-          }`}
-        />
-        <img
-          onClick={() => {
-            onClickHandler("love");
-          }}
-          src="/emoji/love.png"
-          alt=""
-          className={`w-[22px] h-[22px] cursor-pointer transition-all duration-500 delay-75 filter ${
-            value.includes("love") ? "" : "grayscale"
-          }`}
-        />
-        <img
-          onClick={() => {
-            onClickHandler("smile");
-          }}
-          src="/emoji/smile.png"
-          alt=""
-          className={`w-[22px] h-[22px] cursor-pointer transition-all duration-700 delay-100 filter ${
-            value.includes("smile") ? "" : "grayscale"
-          }`}
-        />
-        <img
-          onClick={() => {
-            onClickHandler("sparkles");
-          }}
-          src="/emoji/sparkles.png"
-          alt=""
-          className={`w-[22px] h-[22px] cursor-pointer transition-all duration-1000 delay-150 filter ${
-            value.includes("sparkles") ? "" : "grayscale"
-          }`}
-        />
+        {emojiesArray.map(emoji => {
+          return (
+            <img
+              onClick={() => {
+                onClickHandler(emoji.name);
+              }}
+              src={`/emoji/${emoji.name}.png`}
+              alt={emoji.name}
+              className={`w-[22px] h-[22px] cursor-pointer transition-all duration-300 filter ${
+                value.includes(emoji.name) ? "" : "grayscale"
+              }`}
+            />
+          );
+        })}
       </div>
     </>
   );
